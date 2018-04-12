26b3a639fbb683dc8bbb390e4988ced7cd84a195
liguosongjava <1017140777@qq.com>
2018-04-13 00:50:22 +0800

    添加上传路径配置

--- src/main/java/com/safe/info/core/settings/ServerSettings.java
+++ src/main/java/com/safe/info/core/settings/ServerSettings.java
@@ -5,5 +5,23 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "core")
 public class ServerSettings {
+    private String houseRegImportPath;
 
+    private String houseProImportPath;
+
+    public String getHouseRegImportPath() {
+        return houseRegImportPath;
+    }
+
+    public void setHouseRegImportPath(String houseRegImportPath) {
+        this.houseRegImportPath = houseRegImportPath;
+    }
+
+    public String getHouseProImportPath() {
+        return houseProImportPath;
+    }
+
+    public void setHouseProImportPath(String houseProImportPath) {
+        this.houseProImportPath = houseProImportPath;
+    }
 }
